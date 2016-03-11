14a6795a7620ae6874bab77087991ed5dafbe20c
Noah Springwater <nspringwater@gmail.com>
2016-03-11 18:05:59 -0500

    set up server.js

--- server.js
+++ server.js
@@ -8,4 +8,25 @@ app.use(logger('dev'));
 
 var api_key = process.env.WEATHER_SECRET;
 
-app.set('port', (process.env.PORT || 3000))
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (request, response) {
+  response.render('public/index.html');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Node running on port 3000', app.get('port'));
+});
+
+app.get('/weather/:city', function (req, res) {
+  request("api.openweathermap.org/data/2.5/weather?q=" + req.params.city + "&appid=" + api_key, function(error, response, body){
+    res.send(body);
+  });
+});
+
+app.get('/forecast/:city', function (req, res) {
+  request("api.openweathermap.org/data/2.5/forecast?q=" + req.params.city + "&appid=" + api_key, function (error, response, body) {
+    res.send(body);
+  });
+});
